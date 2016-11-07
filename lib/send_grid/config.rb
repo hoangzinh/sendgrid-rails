4c8fbd90fdac7f460b7ce28db646812ceea52880
KA + Vinh <hoangzinhvn@gmail.com>
2016-11-07 17:26:02 +0700

    Override recipient feature

--- lib/send_grid/config.rb
+++ lib/send_grid/config.rb
@@ -1,7 +1,8 @@
 class SendGrid::Config
-  attr_accessor :dummy_recipient
+  attr_accessor :dummy_recipient, :override_recipient
 
   def initialize
     @dummy_recipient = "dummy@email.com"
+    @override_recipient = nil
   end
 end
--- lib/send_grid/mail_interceptor.rb
+++ lib/send_grid/mail_interceptor.rb
@@ -2,11 +2,19 @@ module SendGrid
   class MailInterceptor
     def self.delivering_email(mail)
       sendgrid_header = mail.instance_variable_get(:@sendgrid_header)
-      sendgrid_header.add_recipients(mail.to)
-      sendgrid_header.add_recipients(mail.cc)
-      sendgrid_header.add_recipients(mail.bcc)
+
+      if override_recipient = SendGrid.config.override_recipient
+        sendgrid_header.add_recipients(override_recipient)
+        mail.header['to'] = override_recipient
+        mail.header['cc'] = nil
+        mail.header['bcc'] = nil
+      else
+        sendgrid_header.add_recipients(mail.to)
+        sendgrid_header.add_recipients(mail.cc)
+        sendgrid_header.add_recipients(mail.bcc)
+      end
+
       mail.header['X-SMTPAPI'] = sendgrid_header.to_json if sendgrid_header.data.present?
-      mail.header['to'] = SendGrid.config.dummy_recipient
     end
   end
 end
